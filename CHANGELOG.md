6fc31f078dedb013e5e9150b206c808f1e2ef166
Pranay Bhaturkar <pbhaturkar5@gmail.com>
2022-04-10 17:25:44 +0530

    docs: ChangeLog add

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
-## 0.0.1
+## 2.0.0 (2022-04-10)
 
-* TODO: Describe initial release.
+##### Documentation Changes
+
+* **Readme file:**  Description updated ([85bfb6db](https://github.com/pranay289/foto_gallery/commit/85bfb6db890c3f4326b80dc8732bbd5cf4246db7))
+
+## 2.0.0 (2022-04-10)
+
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -29,32 +30,34 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<String> imageList = [
-    "https://images.pexels.com/photos/213780/pexels-photo-213780.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg",
     "https://images.pexels.com/photos/2899097/pexels-photo-2899097.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     "https://images.pexels.com/photos/2820884/pexels-photo-2820884.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: ListView.builder(
-            itemCount: imageList.length,
-            itemBuilder: ((context, index) {
-              return Container(
-                height: 300,
-                width: double.infinity,
-                padding: const EdgeInsets.all(30),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: FotoGallery(
-                    image: imageList[index],
-                    imageList: imageList,
-                    imgurl: imageList[index],
-                    index: index,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              );
-            })));
+      appBar: AppBar(),
+      body: ListView.builder(
+        itemCount: imageList.length,
+        itemBuilder: ((context, index) {
+          return Container(
+            height: 300,
+            width: double.infinity,
+            padding: const EdgeInsets.all(30),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: FotoGallery(
+                image: imageList[index],
+                imageList: imageList,
+                imgurl: imageList[index],
+                index: index,
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        }),
+      ),
+    );
   }
 }
